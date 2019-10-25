bce9b3129487e78660150919a49cc8e204034d41
Takexito <Takexito@gmail.com>
2019-10-25 16:52:01 +0300

    layout 2.0.1

--- app/src/main/java/bonch/dev/school/ui/fragments/PasswordFragment.kt
+++ app/src/main/java/bonch/dev/school/ui/fragments/PasswordFragment.kt
@@ -1,10 +1,13 @@
 package bonch.dev.school.ui.fragments
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.Window
 import androidx.fragment.app.DialogFragment
 import bonch.dev.school.R
 import kotlinx.android.synthetic.main.fragment_password.*
@@ -15,9 +18,11 @@ class PasswordFragment : DialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog?.window?.requestFeature(Window.FEATURE_NO_TITLE)
         return inflater.inflate(R.layout.fragment_password, container, false)
     }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
