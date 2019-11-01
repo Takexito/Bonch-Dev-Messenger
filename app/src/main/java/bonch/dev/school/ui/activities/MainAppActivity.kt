e4b33a0bf9b74c952676b6232e2e2d8de5deb365
Takexito <Takexito@gmail.com>
2019-11-01 17:32:41 +0300

    navigation menu

--- app/src/main/java/bonch/dev/school/ui/activities/MainAppActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/MainAppActivity.kt
@@ -2,27 +2,28 @@ package bonch.dev.school.ui.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import bonch.dev.school.R
-import bonch.dev.school.ui.fragments.ChatFragment
 import bonch.dev.school.ui.fragments.PasswordFragment
 import bonch.dev.school.ui.fragments.ProfileFragment
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 
 class MainAppActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_app)
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment, ChatFragment.newInstance())
-            .commit()
-
-    }
-
     fun replaceFragment() {
         supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment, ProfileFragment.newInstance())
+            .replace(R.id.nav_host_fragment, ProfileFragment.newInstance())
             .addToBackStack(null)
             .commit()
     }
@@ -39,5 +40,47 @@ class MainAppActivity : AppCompatActivity() {
         val newFragment = PasswordFragment.newInstance()
         newFragment.show(ft, "dialog")
         Log.d("PassFragment:", "Dialog was show")
+    }
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.nav_host_fragment, ChatFragment.newInstance())
+//            .commit()
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val fab: FloatingActionButton = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_profile
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
     }
 }
