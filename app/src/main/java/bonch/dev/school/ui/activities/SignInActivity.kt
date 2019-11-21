268ac7021561ade0dda5dbf608bf9aeaf63394a5
Takexito <Takexito@gmail.com>
2019-11-21 03:43:38 +0300

    SignIn + SignUp Firebase

--- app/src/main/java/bonch/dev/school/ui/activities/SignInActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/SignInActivity.kt
@@ -2,22 +2,68 @@ package bonch.dev.school.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import bonch.dev.school.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
+
 class SignInActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        mAuth = FirebaseAuth.getInstance()
+        val currentUser = mAuth.currentUser
+        if (currentUser != null) startChat()
 
         sign_in_button.setOnClickListener {
-            startActivity(Intent(this, MainAppActivity::class.java))
+            if(email_sign_in_edit_text.text.toString().length >= 8 && password_sign_in_edit_text.text.toString().length >= 5)
+            signIn(
+                email_sign_in_edit_text.text.toString(),
+                password_sign_in_edit_text.text.toString()
+            )  //TODO: Add more limits
+
+            else{
+                Toast.makeText(
+                    baseContext, "Password length must be >= 5",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
         }
 
         sign_up_button.setOnClickListener {
             startActivity(Intent(this, SignUpActivity::class.java))
         }
     }
+
+    private fun signIn(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("BonchMess", "signInWithEmail:success")
+                    val user = mAuth.currentUser
+                    //updateUI(user)
+                    startChat()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("BonchMess", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    private fun startChat() {
+        startActivity(Intent(this, MainAppActivity::class.java))
+    }
+
 }
--- app/src/main/java/bonch/dev/school/ui/activities/SignUpActivity.kt
+++ app/src/main/java/bonch/dev/school/ui/activities/SignUpActivity.kt
@@ -2,19 +2,67 @@ package bonch.dev.school.ui.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import bonch.dev.school.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        mAuth = FirebaseAuth.getInstance()
+
 
         complete_sign_up_button.setOnClickListener {
-            startActivity(Intent(this, MainAppActivity::class.java))
+            if (email_sign_up_et.text.toString().length >= 8 &&
+                password_sign_up_et.text.toString().length >= 5 &&
+                login_et.text.toString().length >= 3) {
+
+                if (password_sign_up_et.text.toString() == password_confirm_et.text.toString())
+                    signUp(
+                        email_sign_up_et.text.toString(),
+                        password_sign_up_et.text.toString(),
+                        login_et.text.toString()
+                    ) //TODO: Add more limits
+                else Toast.makeText(
+                    baseContext, "Passwords do not match! ",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+            else Toast.makeText(
+                baseContext, "Password length must be > 4 & name > 2",
+                Toast.LENGTH_SHORT
+            ).show()
         }
 
+    }
+
+    private fun signUp(email: String, password: String, name: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("BonchMess", "createUserWithEmail:success")
+                    val user = mAuth.currentUser
+                    user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(name).build())
+                    //updateUI(user)
+                    startActivity(Intent(this, MainAppActivity::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("BonchMess", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
     }
 }
